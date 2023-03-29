793f1fcbdbb809d3038f37e37a123a547ae1664e
Momoko_Iwagami <iwagami.m0@gmail.com>
2023-03-29 13:36:40 +0100

    Milestone 4

--- manual_rps.py
+++ manual_rps.py
@@ -8,26 +8,26 @@ def get_user_choice():
     user = input("What is your choice?")
     return user
 
-def get_winner():
-    computer= get_computer_choice()
-    player= get_user_choice()
-    if computer == player:
+def get_winner(computer_choice, user_choice):
+    computer_choice= get_computer_choice()
+    user_choice= get_user_choice()
+    if computer_choice == user_choice:
         print("It is a tie!")
-    elif player == "Paper":
-        if computer == "Rock":
+    elif user_choice == "Paper":
+        if computer_choice == "Rock":
             print("You won!")
         else:
             print("You lost")
-    elif player == "Scissors":
-        if computer == "Paper":
+    elif user_choice== "Scissors":
+        if computer_choice == "Paper":
             print("You won!")
         else:
             print("You lost")
-    elif player == "Rock":
-        if computer == "Scissors":
+    elif user_choice == "Rock":
+        if computer_choice == "Scissors":
             print("You won!")
         else: 
             print("You lost")
     else:
         print("It is not valid")
-get_winner()  
+get_winner("Rock","Paper")  
