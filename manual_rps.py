5fc3d31efed816a4ad5dd545e1c696ff6efdb228
Momoko_Iwagami <iwagami.m0@gmail.com>
2023-03-29 11:34:17 +0100

    Milestone4

---
+++ manual_rps.py
@@ -0,0 +1,11 @@
+import random
+
+def get_computer_choice():
+    list_1 = ["Rock", "Paper", "Scissors"]
+    print(random.choice(list_1))
+get_computer_choice()
+
+def get_user_choice():
+    user = input("What is your choice?")
+    return user
+get_user_choice()
